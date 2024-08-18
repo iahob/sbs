b4d1f4f6015c008d60e6258534e502d5258f5565
yuhaibo <yuwenhaibo1@gmail.com>
2024-08-18 22:04:01 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,25 +6,17 @@ if [ ! -f $LOCKFILE ];then
     #generate uuid
     echo "Generate UUID..."
     sing-box generate uuid > config/uuid
-
-
     #generate Public & Private keys
     echo "Generate public & private keys..."
     sing-box generate reality-keypair > config/keys
-
     #Create files with Public & Private keys
     awk '/PublicKey/{print $2}' config/keys > config/public
     awk '/PrivateKey/{print $2}' config/keys > config/private
-
     UUID=$(cat config/uuid)
     PRIVATE=$(cat config/private)
-
+    CONFIG_FILE="config/config.json"
     #set uuid in config.json
-    sed -i 's/<uuid>'/${UUID}/g' config/config.json
-
-    #set private key in config.json
-    sed -i 's/<private_key>/${PRIVATE}/' config/config.json
-
+    sed -i "s/<uuid>/${UUID}/g; s/<private_key>/${PRIVATE}/g" "$CONFIG_FILE"
     #create lockfile
     touch $LOCKFILE
 fi

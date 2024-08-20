d67c2253acf968424b6cd0e6f7ba5c6ce9585f08
yuhaibo <yuwenhaibo1@gmail.com>
2024-08-20 20:49:48 +0800

    update

--- get-client-qr.sh
+++ get-client-qr.sh
@@ -5,5 +5,5 @@ PUB_KEY=$(cat config/public)
 SHORT_ID=$(cat config/short_id)
 SNI=$(cat config/sni)
 
-echo "vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${SHORT_ID}#MyVLESS" > config/client_qr.txt
+echo "vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${SHORT_ID}#VLESS-Vision-REALITY" > config/client_qr.txt
 qrencode -t ansiutf8 < config/client_qr.txt
